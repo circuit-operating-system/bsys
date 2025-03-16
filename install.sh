178c388195c53f93c7d893ccc63dd5024fc7e6a5
Sandwhiched <sandwh1ched.real@gmail.com>
2025-03-16 18:01:04 -0500

    Make scripts POSIX-compliant
    I know that this is a good thing to do and I should have done it anyway, but I had no choice since I switched to FreeBSD :) (I probably would have done it if someone made an issue or over time, I completely forgot about POSIX.)

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # Return codes:
 #   0 - OK
@@ -9,27 +9,27 @@
 set -e
 
 # Check if we have root privileges
-if [[ ! $(whoami) == "root" ]]; then
+if [ "$(whoami)" != "root" ]; then
     echo "(X) Installer script requires root privileges." >&2
     exit 1
 fi
 
 # Check if -u / --uninstall option is passed
-if [[ "$1" == "-u" || "$1" == "--uninstall" ]]; then
+if [ "$1" = "-u" ] || [ "$1" = "--uninstall" ]; then
     rm -rf /opt/bsys
     rm -f /bin/bsys
     echo "=> Uninstall successful."
-    exit
+    exit 0
 fi
 
 # Check if files are stored at the install dir
-if [[ -d "/opt/bsys" ]]; then
+if [ -d "/opt/bsys" ]; then
     echo "(X) Conflict at install directory. Please move /opt/bsys contents and remove the directory." >&2
     exit 2
 fi
 
 # Make the install directory
-mkdir /opt/bsys /opt/bsys/bin /opt/bsys/etc
+mkdir -p /opt/bsys/bin /opt/bsys/etc
 
 # Copy files to install dir
 cp ./bin/bsys /opt/bsys/bin/bsys
@@ -41,3 +41,4 @@ chmod +x /bin/bsys
 
 # Success message
 echo "=> Successful! Use $0 -u to uninstall."
+exit 0
